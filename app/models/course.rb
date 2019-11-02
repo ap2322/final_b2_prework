37dd94eced86fde9e56e941c9a29d6126f4ff1b1
Alice Post <alicep2322@gmail.com>
2019-11-02 16:15:29 -0600

    Add instance method to find the grade of a particular course and student

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,8 @@ class Course < ApplicationRecord
 
   has_many :course_students
   has_many :students, through: :course_students
+
+  def student_grade(student_id)
+    course_students.find_by(student_id: student_id).grade
+  end
 end
