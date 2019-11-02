80b642f662941e9f66c6d7bbec62b5b5d4eb4805
Alice Post <alicep2322@gmail.com>
2019-11-02 16:15:53 -0600

    Add Student Show page and test

--- spec/features/students/show_spec.rb
+++ spec/features/students/show_spec.rb
@@ -2,16 +2,28 @@ require 'rails_helper'
 
 describe 'on the student show page /students/studentid' do
   describe 'as a user' do
+    before(:each) do
+      @student = create(:student, :with_courses, cs_count: 3, name: "Wiz Kid")
+      @courses = @student.courses
+    end
+
+    after(:each) do
+      CourseStudent.all.delete_all
+      Course.all.delete_all
+      Student.all.delete_all
+    end
+
     it 'I see the student name, courses, and grades for each course' do
-      
+      visit "/students/#{@student.id}"
+
+      expect(page).to have_content("Wiz Kid")
+
+      @courses.each do |course|
+        within "#course-#{course.id}" do
+          expect(page).to have_content(course.course_name)
+          expect(page).to have_content(course.student_grade(@student.id))
+        end
+      end
     end
   end
 end
-
-# User Story 1, Student Show Page
-# As a user,
-# When I visit a student show page
-# Then I see
-# - the student's name
-# - the name of each course the student is enrolled in
-# - the student's grade for each course
